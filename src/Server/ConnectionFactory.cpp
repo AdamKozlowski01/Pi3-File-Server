eb2ef96876f7447d84d063178157d0bab432beb5
Adam Kozlowski <Akozlowski01@gmail.com>
2016-09-07 00:24:55 -0500

    Factory Update

--- src/Server/ConnectionFactory.cpp
+++ src/Server/ConnectionFactory.cpp
@@ -15,6 +15,27 @@ public:
 
 };
 
+class StandardConnection : public Connection{
+private:
+	int connection_fd;
+
+protected:
+public:
+	Connection(int fileDescrptr){}
+	~Connection(){}
+	void connectionProtocol(){
+		//set up key exchange for the session
+		
+		//ask for credentials
+
+		//check credentials or if create account mode, ask to create credentials
+
+		//send names of all folders in user's file
+		//process requests until connection time out or ends
+		
+	}
+};
+
 class ConnectionFactory{
 
 private:
@@ -24,12 +45,11 @@ public:
 
 };
 
-class ConnectionFactory_v1{
+class ConnectionFactory_v1 : public ConnectionFactory{
 
 private:
 protected:
 public:
-
 	ConnectionFactory(){}
 	~ConnectionFactory(){}
 	Connection* createConnection(int fileDescrptr){
